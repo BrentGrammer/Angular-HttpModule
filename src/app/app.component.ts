b317a94afaa5a338c1df2081c2e62388783be3e1
Brent Marquez <brentonmarquez@gmail.com>
2019-11-11 21:38:39 -0700

    add loading placeholder while fetching data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from "./post.model";
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -45,6 +46,8 @@ export class AppComponent implements OnInit {
    *       This allows you to omit annotating the argument in operators or in the subscribe callback
    */
   fetchPosts() {
+    this.isFetching = true;
+
     this.http
       .get<{ [key: string]: Post }>(
         "https://angular-recipes-app-584be.firebaseio.com/posts.json"
@@ -63,6 +66,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
